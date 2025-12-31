68e1d74e5a13854090130b4dbdd5a5c960a9c28b
Kodux78k <koduxlive@gmail.com>
2025-12-31 20:24:47 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'pwa-dualinfodose-78Core-v0';
+const ASSETS = [
+    './',
+    'index.html',
+    'manifest.json',
+    'https://api.dicebear.com/9.x/bottts-neutral/svg?seed=HaskiCore'
+];
+
+// Instalação: Cache inicial
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Ativação: Limpeza de caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
+
+// Fetch: Stale-While-Revalidate (Performance + Offline)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      const fetchPromise = fetch(event.request).then(networkResponse => {
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
